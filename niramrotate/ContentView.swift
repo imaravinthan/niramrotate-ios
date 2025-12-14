83222e29b98de2cf6dda156f67a52cb4ee155d01
imaravinthan <aravinthan.selvaraj@icloud.com>
2025-12-14 12:42:10 +0530

    Initial Commit

---
+++ niramrotate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  niramrotate
+//
+//  Created by aravinthan.selvaraj on 14/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Aravinthan!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ niramrotate/niramrotateApp.swift
@@ -0,0 +1,17 @@
+//
+//  niramrotateApp.swift
+//  niramrotate
+//
+//  Created by aravinthan.selvaraj on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct niramrotateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
